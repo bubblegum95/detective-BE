df8b84043a38e4db12991f8364a171df53e1e3fe
HA0N1 <intor48@gmail.com>
2024-05-20 22:06:37 +0900

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-Ctrl + -> Editor: Default Formatter 검색 -> Prettier로 설정
-Editor: Format on save mode 체크
+### 프리티어 설정 확인하세여
+
+1. Ctrl + -> Editor: Default Formatter 검색 -> Prettier로 설정
+2. Editor: Format on save mode 체크
+
+### 프로젝트 시작 시 항상 할 것!
+
 docker-compose up
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
+const typeOrmModuleOptions = {
+  useFactory: async (configService: ConfigService): Promise<TypeOrmModuleOptions> => ({
+    namingStrategy: new SnakeNamingStrategy(), // 자동으로 DB에 스네이프 케이스로
+    type: 'postgres',
+    host: configService.get('POSTGRES_HOST'),
+    port: configService.get('POSTGRES_PORT'),
+    username: configService.get('POSTGRES_USER'),
+    password: configService.get('POSTGRES_PASSWORD'),
+    database: configService.get('POSTGRES_DB'),
+    entities: [],
+    synchronize: configService.get('POSTGRES_SYNC'),
+    logging: true, // row query 출력
+  }),
+  inject: [ConfigService],
+};
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync(typeOrmModuleOptions),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
