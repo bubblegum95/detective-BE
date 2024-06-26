cb1504e914bd194cb605ac8ee930fc74b18c879c
bubblegum95 <a82727282@gmail.com>
2024-06-26 20:21:16 +0900

    save

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -13,6 +13,12 @@ import { Server, Socket } from 'socket.io';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { UserInfo } from '../utils/user-info.decorator';
 import { User } from '../user/entities/user.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { Message } from './entities/message.entity';
+import { Model } from 'mongoose';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DataSource, Repository, getConnection } from 'typeorm';
+import { Room } from './entities/room.entity';
 
 @UseGuards(JwtAuthGuard)
 @WebSocketGateway()
@@ -20,60 +26,39 @@ export class ChatGateway {
   @WebSocketServer()
   server: Server;
 
-  afterInit(server: Server) {
-    console.log('Init');
-  }
-
-  handleConnection(client: Socket, ...args: any[]) {
-    console.log('Client connected:', client.id);
-  }
-
-  handleDisconnect(client: Socket) {
-    console.log('Client disconnected:', client.id);
-  }
-
-  requestUserInfo(client: Socket) {
-    client.emit('requestUserInfo');
-  }
-
-  private rooms = new Map<string, Set<string>>(); // 룸 관리를 위한 맵
+  constructor(
+    @InjectModel('Message') private readonly messageModel: Model<Message>,
+    @InjectRepository(Room) private readonly roomRepository: Repository<Room>,
+    private readonly dataSource: DataSource,
+  ) {}
 
   @SubscribeMessage('createRoom')
-  handleCreateRoom(client: Socket, roomId: string, @UserInfo() user: User): void {
-    // 새로운 룸 생성
-    this.rooms.set(roomId, new Set<string>());
-    console.log(`Room ${roomId} created.`);
-    console.log('user: ', user);
-  }
+  async handleCreateRoom(
+    client: Socket,
+    data: { message: string; url: string },
+    @UserInfo() user: User,
+  ) {
+    const room = await this.roomRepository.save({});
+    const roomId = room.id.toString();
 
-  @SubscribeMessage('inviteToRoom')
-  handleInviteToRoom(client: Socket, data: { roomId: string; invitedUserId: string }): void {
-    const { roomId, invitedUserId } = data;
-    if (this.rooms.has(roomId)) {
-      // 초대된 사용자를 룸에 추가
-      this.rooms.get(roomId).add(invitedUserId);
-      console.log(`User ${invitedUserId} invited to room ${roomId}.`);
-      // 초대된 사용자에게 초대 메시지 전송 (예시)
-      this.server.to(invitedUserId).emit('roomInvitation', { roomId });
-    } else {
-      console.log(`Room ${roomId} does not exist.`);
-    }
+    room.user.push(user, recipient);
+    await this.roomRepository.save(room);
+
+    client.join(roomId);
+    client.emit('create roomm and join', roomId);
+
+    console.log(`Room ${roomId} created.`);
   }
 
   @SubscribeMessage('joinRoom')
-  handleJoinRoom(client: Socket, roomId: string): void {
-    // 사용자가 룸에 입장
-    client.join(roomId);
+  async handleJoinRoom(client: Socket, roomId: string): void {
+    const room = Number(roomId);
+    const existUserInRoom = client.join(roomId);
     console.log(`User ${client.id} joined room ${roomId}.`);
   }
 
-  private removeClientFromRooms(clientId: string): void {
-    this.rooms.forEach((users, roomId) => {
-      if (users.has(clientId)) {
-        users.delete(clientId);
-        console.log(`User ${clientId} left room ${roomId}.`);
-      }
-    });
+  async existClientFromRooms(clientId: string, roomId: string): void {
+    const foundRoom = await this.roomRepository.update({ where: { roomId } });
   }
 
   @SubscribeMessage('message')
