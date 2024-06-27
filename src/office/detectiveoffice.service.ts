1bb9552e56b06476c5f0cfc89d71e53543573ba3
unknown <woo00123@naver.com>
2024-06-27 21:35:52 +0900

    :recycle: Refactor: 오피스 생성 api 작성 중..

--- src/office/detectiveoffice.service.ts
+++ src/office/detectiveoffice.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Detective } from 'src/user/entities/detective.entity';
+import { Repository } from 'typeorm';
+import { DetectiveOffice } from './entities/detective-office.entity';
+import { CreateDetectiveOfficeDto } from './dto/create-office.dto';
 
 @Injectable()
-export class DetectiveofficeService {}
+export class DetectiveofficeService {
+  constructor(
+    @InjectRepository(Detective)
+    private detectiveRepo: Repository<Detective>,
+    @InjectRepository(DetectiveOffice)
+    private detectiveOfficeRepo: Repository<DetectiveOffice>,
+  ) {}
+
+  async createDetectiveOffice(
+    createDetectiveOfficeDto: CreateDetectiveOfficeDto,
+  ): Promise<DetectiveOffice> {
+    const { userId, name, location, description, founded, businessRegistrationNum } =
+      createDetectiveOfficeDto;
+
+    const detective = await this.detectiveRepo.findOne({ where: { userId } });
+
+    if (detective.position === 'employee') {
+      throw new Error('고용인만 오피스를 생성할 수 있습니다.');
+    }
+
+    const detectiveOffice = this.detectiveOfficeRepo.create({
+      ownerId: detective.id,
+      locationId: location.id
+      name,
+      description,
+      founded,
+      businessRegistrationNum,
+    });
+
+    return this.detectiveOfficeRepo.save(detectiveOffice);
+  }
+
+  // 오피스 등록 요청
+  async requestRegistration() {}
+}
