d0540f415758d2c754dffde4c597948560795604
HA0N1 <intor48@gmail.com>
2024-07-24 17:36:42 +0900

    :wrench: Chore: Winston 관련 패키지 설치

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,12 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-<<<<<<< HEAD
 import cookieParser, { signedCookie } from 'cookie-parser';
 import { Server } from 'socket.io';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { RedisIoAdapter } from './redis/redis-io-adapter';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
-
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: false, // 기본 로거를 비활성화! winston으로 사용할거임
@@ -17,15 +16,8 @@ async function bootstrap() {
   const winstonLogger = app.get(WINSTON_MODULE_NEST_PROVIDER);
   app.useLogger(winstonLogger);
 
-=======
-import cookieParser from 'cookie-parser';
-import { RedisIoAdapter } from './redis/redis-io.adapter';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-
-async function bootstrap() {
   const logger = new Logger(bootstrap.name);
-  const app = await NestFactory.create(AppModule);
->>>>>>> 41d0aad6cc4e410a9a80d00a0440cdb1b502ec8d
+
   const configService = app.get(ConfigService);
   const port = configService.get<number>('SERVER_PORT');
   const option = {
@@ -75,9 +67,7 @@ async function bootstrap() {
 
   //Http 서버 시작
   await app.listen(port);
-<<<<<<< HEAD
   winstonLogger.log(`Application is running on: ${await app.getUrl()}`, 'Bootstrap');
-=======
 
   // 마이크로서비스 설정
   const microservice = app.connectMicroservice<MicroserviceOptions>({
@@ -91,6 +81,5 @@ async function bootstrap() {
   // 마이크로서비스 시작
   await app.startAllMicroservices();
   logger.log('Microservice is listening');
->>>>>>> 41d0aad6cc4e410a9a80d00a0440cdb1b502ec8d
 }
 bootstrap();
