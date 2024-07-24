cb544564e24add0c5dab9b3ff1594ba9d1e6869b
HA0N1 <intor48@gmail.com>
2024-07-24 17:28:09 +0900

    :wrench: Chore: Winston 관련 패키지 설치

---
+++ config/logger.config.ts
@@ -0,0 +1,3 @@
+import { WinstonModule } from 'nest-winston';
+import * as winstonDaily from 'winston-daily-rotate-file';
+import * as winston from 'winston';
--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,15 @@ import cookieParser, { signedCookie } from 'cookie-parser';
 import { Server } from 'socket.io';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { RedisIoAdapter } from './redis/redis-io-adapter';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: false, // 기본 로거를 비활성화! winston으로 사용할거임
+  });
+  const winstonLogger = app.get(WINSTON_MODULE_NEST_PROVIDER);
+  app.useLogger(winstonLogger);
+
   const configService = app.get(ConfigService);
   const port = configService.get<number>('SERVER_PORT');
   const option = {
@@ -60,5 +66,6 @@ async function bootstrap() {
   app.useWebSocketAdapter(redisIoAdapter);
 
   await app.listen(port);
+  winstonLogger.log(`Application is running on: ${await app.getUrl()}`, 'Bootstrap');
 }
 bootstrap();
